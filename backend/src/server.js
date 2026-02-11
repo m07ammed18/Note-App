1178f3abec08ab2c81b30309c70f3aa1e569c252
Mohammed Belhadj <bel7adj08@gmail.com>
2026-02-11 02:28:45 +0100

    Refactor server configuration and enhance axios setup for environment-based API URLs

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import notesRoutes from "./routes/noteRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -10,16 +11,29 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // Middleware
-app.use(
+if (process.env.NODE_ENV !== "production") {
+  app.use(
   cors({
     origin: "http://localhost:5173",
   })
 );
+}
+
 app.use(express.json()); // This middleware will parse JSON badies: req.body
 app.use(ratelimiter);
 app.use("/api/notes", notesRoutes);
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+});
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
--- frontend/src/libs/axios.ts
+++ frontend/src/libs/axios.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-const api = axios.create(
-  {
-    baseURL: "http://localhost:5001/api"
-  }
-)
+const MODE = import.meta.env.MODE || "production";
+const BASE_URL = MODE === "development" ? "http://localhost:5001/api" : "/api";
+
+const api = axios.create({
+  baseURL: BASE_URL,
+});
 export default api;
